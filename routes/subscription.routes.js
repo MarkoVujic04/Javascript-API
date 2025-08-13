1559b8888afef7b97bb4e042e95a5137cfe987ae
markovujic04 <116155074+MarkoVujic04@users.noreply.github.com>
2025-08-13 23:59:04 +0200

    Setup

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.post('/', (req, res) => res.send({title: 'GET all subscriptions'}));
+subscriptionRouter.post('/', (req, res) => res.send({title: 'GET all subscriptionss'}));
 
 
 export default subscriptionRouter;
