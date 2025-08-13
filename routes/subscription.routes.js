8bbc022507511c6ad9da7887b4842656ce2d1664
markovujic04 <116155074+MarkoVujic04@users.noreply.github.com>
2025-08-14 00:01:17 +0200

    First Commit

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.post('/', (req, res) => res.send({title: 'GET all subscriptionss'}));
+subscriptionRouter.post('/', (req, res) => res.send({title: 'GET all subscriptions'}));
 
 
 export default subscriptionRouter;
