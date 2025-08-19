990c7ab5b0bced4ba434998f15f697e9a0419334
markovujic04 <116155074+MarkoVujic04@users.noreply.github.com>
2025-08-19 15:46:51 +0200

    Connected to simplistic frontend

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import connectToDatabase from './database/mongodb.js'
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+import cors from "cors";
 
 const app = express();
 
@@ -14,6 +15,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(arcjetMiddleware);
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+  methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
